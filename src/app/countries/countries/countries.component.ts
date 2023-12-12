750a3eb487079ef8844ffbf964a594d4dd6619e5
Robert Thomas <robertkylethomas@gmail.com>
2023-12-12 15:14:39 +0200

    Added styles and got the countries to pull from the server

--- src/app/countries/countries/countries.component.ts
+++ src/app/countries/countries/countries.component.ts
@@ -2,50 +2,60 @@ import { environment } from './../../../environments/environment';
 import { tap } from 'rxjs/operators';
 import { Component, Inject, OnInit } from '@angular/core';
 import { CountriesService } from '../countries.service';
-import { Observable, of, map, mergeMap, distinct, toArray, mergeAll } from 'rxjs';
+import {
+  Observable,
+  of,
+  map,
+  mergeMap,
+  distinct,
+  toArray,
+  mergeAll,
+} from 'rxjs';
 import { Country } from '../Country';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
-  styleUrl: './countries.component.css'
+  styleUrl: './countries.component.css',
 })
 export class CountriesComponent implements OnInit {
+  constructor(
+    private countriesService: CountriesService,
+    private router: Router
+  ) {}
 
-
-  constructor(private countriesService: CountriesService, private router: Router){}
-
-  countries$:Observable<Country[]> = of([]);
+  countries$: Observable<Country[]> = of([]);
   filteredCountries$: Observable<Country[]> = this.countries$;
 
   ngOnInit(): void {
-      this.countries$ = this.countriesService.getAllCountries();
-      console.log('these are teh regions')
-
+    this.countries$ = this.countriesService.getAllCountries();
+    console.log('these are teh regions');
   }
 
-  getFilteredCountries(event: string){
-
+  getFilteredCountries(event: string) {
     this.filteredCountries$ = this.countries$.pipe(
       map((countries: Country[]) => {
-        return countries.filter((country: Country) => country.name.common.toLowerCase().includes(event))
-      }),
-
-    )
+        return countries.filter((country: Country) =>
+          country.name.common.toLowerCase().includes(event)
+        );
+      })
+    );
   }
 
-  filterByRegion(event: any){
-    console.log(event)
-
+  filterByRegion(event: any) {
     this.filteredCountries$ = this.countries$.pipe(
-      map((countries: Country[]) => countries.filter((country: Country) => country.region.toLowerCase() === event.toLowerCase())),
-      tap(res => console.log(res))
-    )
+      map((countries: Country[]) =>
+        countries.filter(
+          (country: Country) =>
+            country.region.toLowerCase() === event.toLowerCase()
+        )
+      ),
+      tap((res) => console.log(res))
+    );
   }
 
-  routeTo(countryCode: string){
-    this.router.navigate([countryCode])
+  routeTo(countryCode: string) {
+    this.router.navigate([countryCode]);
   }
-
 }
--- src/app/countries/country-detail/country-detail.component.ts
+++ src/app/countries/country-detail/country-detail.component.ts
@@ -1,22 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, forkJoin } from 'rxjs';
+import {
+  combineLatestAll,
+  map,
+  mergeAll,
+  switchMap,
+  tap,
+  toArray,
+} from 'rxjs/operators';
 import { Country } from '../Country';
 import { CountriesService } from '../countries.service';
 
 @Component({
   selector: 'app-country-detail',
   templateUrl: './country-detail.component.html',
-  styleUrl: './country-detail.component.css'
+  styleUrl: './country-detail.component.css',
 })
 export class CountryDetailComponent implements OnInit {
-
-  constructor(private activatedRoute: ActivatedRoute, private countryService: CountriesService){}
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countryService: CountriesService
+  ) {}
   countryCode: string = this.activatedRoute.snapshot.params['id'];
 
-  country$: Observable<Country> = this.countryService.getCountryByCountryCode(this.countryCode);
-  ngOnInit(): void {
+  country$: Observable<Country> = this.countryService.getCountryByCountryCode(
+    this.countryCode
+  );
 
-  }
+  neighbours$: Observable<Country[]> = this.country$.pipe(
+    map((country: Country) => country.borders),
+    switchMap((neighbouringCountryCodes: string[]) =>
+      neighbouringCountryCodes.map((neighbouringCountryCode: string) =>
+        this.countryService.getCountryByCountryCode(neighbouringCountryCode)
+      )
+    ),
+    combineLatestAll()
+  );
 
+  ngOnInit(): void {}
 }
